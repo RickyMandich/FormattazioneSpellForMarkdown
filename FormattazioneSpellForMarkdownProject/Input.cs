408d1659a1dc6cee081677ea64e16951ca88fe4c
RickyMandich <ricky.mandich@gmail.com>
2026-01-12 19:16:24 +0100

    aggiornamento 2026 01 12 19:16

---
+++ FormattazioneSpellForMarkdownProject/Input.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace FormattazioneSpellForMarkdownProject {
+
+    internal class Input
+    {
+        static void WriteColored(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static string GetString(string s)
+        {
+            WriteColored(s, ConsoleColor.Blue);
+            return GetString();
+        }
+
+        public static string GetString()
+        {
+            return Console.ReadLine();
+        }
+
+        public static bool GetBool(string s)
+        {
+            Console.WriteLine(s);
+            return GetBool();
+        }
+
+        public static bool GetBool()
+        {
+            try
+            {
+                return Boolean.Parse(GetString());
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("devi inserire un valore booleano\t(true per sì e false per no)");
+                return GetBool();
+            }
+        }
+
+        public static int GetInt(int min, string s)
+        {
+            int a = GetInt(s);
+            if(a < min)
+            {
+                Console.Error.WriteLine($"devi inserire un numero intero maggiore o uguale a {min}");
+                return GetInt(min, s);
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        public static int GetInt(int min, int max, string s)
+        {
+            int a = GetInt(min, s);
+            if(a > max)
+            {
+                Console.Error.WriteLine($"devi inserire un numero intero compreso tra {min} e {max}");
+                return GetInt(min, max, s);
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        public static int GetInt(string s = "")
+        {
+            try
+            {
+                return Int32.Parse(GetString(s));
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("devi inserire un numero intero");
+                return GetInt();
+            }
+            catch (OverflowException)
+            {
+                Console.Error.WriteLine("devi inserire un numero intero di massimo 9 cifre");
+                return GetInt();
+            }
+        }
+
+        public static void PauseBeforeExit()
+        {
+            Console.WriteLine("premi invio per uscire...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FormattazioneSpellForMarkdownProject/data/md/2026-01-12_16-30-27finalPartNecromant.md
@@ -0,0 +1,28 @@
+# SKELETAL SPROUTING
+*3° livello, Trasmutazione*
+
+- **Tempo di lancio:** 1 azione
+- **Raggio d'azione:** 18m
+- **Componenti:** V, S, M (a fingerbone)
+- **Durata:** Istantaneo
+
+You afflict up to 3 targets which possess skeletons. and are within a 20' cube. Their bones quiver and sprout random projections within their body. causing crippling pain and injury. Targets of this spell take 4d6 slashing damage and are Restrained. A successful Constitution save reduces the damage by half and negates the Restrained condition. A Restrained target may save again at the end of each of its turn to end the restraint effect.
+
+**Ai livelli superiori:** Casting this spell with a higher level spell slot increases the number of targets affected by 1 per level.
+
+**Classi:** 
+
+---
+# SHOWER OF SHARDS
+*4° livello, Evocazione*
+
+- **Tempo di lancio:** 1 azione
+- **Raggio d'azione:** 18m
+- **Componenti:** V, S, M (a bone knife).
+- **Durata:** Istantaneo
+
+The bone dagger in your hand sprouts four copies of itself out of its blade, which then sprout their own copies until you momentarily hold a hundred bone daggers joined together by thin rods of bone. They then fling themselves at your foes all at once, dealing 8d6 piercing damage in a 45' cone or in a 60'×10' line.A successful Dexterity save halves the damage.
+
+**Classi:** 
+
+---
