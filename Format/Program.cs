c79e1ee5ad7286346a5824ab20abb73976b81dad
RickyMandich <ricky.mandich@gmail.com>
2026-01-27 11:07:01 +0100

    aggiornamento 2026 01 27 11:07

---
+++ Format/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Format;
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] commands = { "spell", "help" };
+        if(commands.Contains(args.ElementAtOrDefault(0)))
+        {
+            
+        }
+        else
+        {
+            Console.WriteLine($"Command {args.ElementAtOrDefault(0)} not found");
+        }
+    }
+
+    static void Spell(string[] args)
+    {
+        Console.WriteLine("Inside Spell method.");
+        string[] options = { "add", "remove", "edit" };
+    }
+}
